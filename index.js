9780b0bf1163025c75329a719503acc32500a95c
Your Name <liuj725@newschool.edu>
2023-11-21 18:10:34 -0500

    cat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import the express library and assign it to a variable
 import express from 'express'
 
+const getRandomCat = require('random-cat-img');
 // Create an instance of an express application 
 const app = express()
 
@@ -8,11 +9,21 @@ const app = express()
 const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
-app.get('/', (req, res) => {
-  res.send("HiHi~ Please post a moon and stars emojis.")
-})
+app.get('/', async (req, res) => {
+  try {
+    // Fetch a random cat image
+    const data = await getRandomCat();
+    const catImageUrl = data.message;
 
-// Set the application to listen a port
+    // Send a response with the cat image URL
+    res.send(`<img src="${catImageUrl}" alt="Random Cat Image">`);
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching random cat image:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}` )
-})
+  console.log(`Example app listening on port ${port}`);
+});
