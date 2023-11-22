1bead134a19e65a53b1c0528be9ee4049047940c
Your Name <liuj725@newschool.edu>
2023-11-21 19:47:07 -0500

    n

--- index.js
+++ index.js
@@ -1,58 +1,26 @@
 // Import the express library and assign it to a variable
-// import express from 'express'
-
-// const randomDog = require('random.dog.js');
-// Create an instance of an express application 
-// const app = express()
-
-// Set the port the application will be running on
-// const port = process.env.PORT || 3001
-
-// Set up a response for the root path of the application
-// app.get('/', async (req, res) => {
-//   try {
-//     const randomDogApi = randomDog.api();
-//     const dogData = await randomDogApi.getDog();
-//     const dogImageUrl = dogData.url;
-
-    // Send a response with the cat image URL
-  //   res.send(`<img src="${dogImageUrl}" alt="Random Dog Image">`);
-  // } catch (error) {
-    // Handle errors
-//     console.error('Error fetching random cat image:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
-// Import the express library and assign it to a variable
-import express from 'express';
-import randomDog from 'random.dog.js';
+import express from 'express'
+import randomDog from 'random.dog';
 
 // Create an instance of an express application 
-const app = express();
+const app = express()
 
 // Set the port the application will be running on
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
 app.get('/', async (req, res) => {
   try {
-    // Create an instance of the random.dog.js API
+    // Fetch a random cat image
     const randomDogApi = randomDog.api();
-
-    // Fetch a random dog image
     const dogData = await randomDogApi.getDog();
     const dogImageUrl = dogData.url;
-
-    // Send a response with the dog image URL
+    
+    // Send a response with the cat image URL
     res.send(`<img src="${dogImageUrl}" alt="Random Dog Image">`);
   } catch (error) {
     // Handle errors
-    console.error('Error fetching random dog image:', error);
+    console.error('Error fetching random cat image:', error);
     res.status(500).send('Internal Server Error');
   }
 });
